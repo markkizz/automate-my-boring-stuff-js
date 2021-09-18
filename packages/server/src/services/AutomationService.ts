fa373395e0b5dea67de4b3810d4473dbe87af06c
markkizz <mark.pattanapara@gmail.com>
2021-09-18 21:53:11 +0700

    fix: automation puppeteer args for cloud

--- packages/server/src/services/AutomationService.ts
+++ packages/server/src/services/AutomationService.ts
@@ -7,7 +7,7 @@ import automation, { WorkinStatus } from "@automation/browser-automation";
 export class AutomationService {
 
   public async automateMeanGoogleForm() {
-    const urlScraped = await automation.meanGoogleFormAutomation(WorkinStatus.In)
+    const urlScraped = await automation.meanGoogleFormAutomation(WorkinStatus.In, { args: ["--no-sandbox", "--disable-setuid-sandbox"] })
     console.log("------ AutomationService -----")
     console.log(urlScraped)
     return urlScraped
