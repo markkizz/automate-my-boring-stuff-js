ef10225c7617bbf576e1bec53a039460ddbb5a54
markkizz <mark.pattanapara@gmail.com>
2021-09-19 01:33:40 +0700

    feat: clean app running code

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,29 +1,10 @@
 import "reflect-metadata"
 import dotenv from "dotenv"
-// import automation, { WorkinStatus } from "@markkizz/browser-automation";
-import { NestFactory } from "@nestjs/core";
-import { ExpressAdapter, NestExpressApplication } from "@nestjs/platform-express";
-import { AppModule } from "./Application/App.module";
-import express, { Express } from "express";
+import express from "express";
+import { Application } from "./Application/Application"
 
 dotenv.config()
 
 const server = express();
-
-const createNestServer = async (expressInstance: Express) => {
-  try {
-    const app = await NestFactory.create<NestExpressApplication>(
-      AppModule,
-      new ExpressAdapter(expressInstance)
-    );
-    await app.init()
-    const port = process.env.PORT || 3000
-    app.listen(port)
-    console.log(`Server started on port ${port}`)
-  } catch (error) {
-    console.log("[error] Server broken")
-    console.log(error)
-  }
-}
-
-createNestServer(server)
+const app = new Application(server)
+app.start(process.env.PORT || 3000)
