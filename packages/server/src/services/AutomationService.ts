e5208927dbfd817b9d77e42dfd9a354775c69b05
markkizz <mark.pattanapara@gmail.com>
2021-09-19 15:40:46 +0700

    feat: add env target google form url

--- packages/server/src/services/AutomationService.ts
+++ packages/server/src/services/AutomationService.ts
@@ -7,7 +7,10 @@ import automation, { WorkinStatus } from "@automation/browser-automation";
 export class AutomationService {
 
   public async automateMeanGoogleForm(workingStatus: WorkinStatus) {
-    const scraped = await automation.meanGoogleFormAutomation(workingStatus, { args: ["--no-sandbox", "--disable-setuid-sandbox"] })
+    const scraped = await automation.meanGoogleFormAutomation(workingStatus, {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      targetUrl: process.env.MEAN_GOOGLE_FORM_URL
+    })
     return scraped
   }
 
